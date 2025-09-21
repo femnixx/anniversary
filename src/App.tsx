a94a3e86c2218489c7472c7d3c24b365933abdd7
femnixx <femnitality@gmail.com>
2025-09-21 11:13:45 +0700

    Started with some elements slicing

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+        <h1>Happy Anniversary</h1>
       <div>
-        <p>Anniversary special</p>
+        <p>I LOVE</p>
+        <h1>YOU</h1>
+      </div>
+      
+      <div>
+        <h1>FIRST MEET</h1>
+        <p>From first meeting because I didn't bring my almameter to the time we met in the open house, my eyes have been opened to how beautiful and kind you are. From just as friends to someone special, I've always loved you, and nothing will ever change that.</p>
       </div>
     </>
   )
