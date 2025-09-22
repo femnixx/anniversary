dbda5479fb45998b4236ad13a28e07589df4f331
femnixx <femnitality@gmail.com>
2025-09-22 20:24:55 +0700

    trying to figure out what works

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,24 @@ import background from "./assets/background.jpg"
 function App() {
   return (
     <>
-     <div className='relative w-full h-screen bg-blue-300'>
-        <p className='font-greatvibes'>Happy <br /> Anniversary</p>
-        <p>Here's to the first year of our relationship</p>
-        {/* decoration like clouds */}
-        <div></div>
-        {/* timeline of us */}
-        <div>
-
+     <div className='relative w-full h-screen background-cover background-center bg-[url(./assets/bluebackground.jpg)]'>
+       {/* background opacity */}
+       <div className='bg-black w-full h-full opacity-10 z-0 absolute'></div>
+        {/* content */}
+        <div className='relative z-20'>
+          <p className='font-greatvibes text-7xl text-white'>Happy <br /> Anniversary</p>
+          <p>Here's to the first year of our relationship</p>
+          {/* decoration like clouds */}
+          <div></div>
+          {/* timeline of us */}
+          <div>
+          </div>
+          {/* favorite pictures */}
+          <div></div>
+          {/* cinnamoroll pop up */}
+          <p>Thank you for being with me</p>
+          <p>Here's some cute things to do here!</p>
         </div>
-        {/* favorite pictures */}
-        <div></div>
-        {/* cinnamoroll pop up */}
-        <p>Thank you for being with me</p>
-        <p>Here's some cute things to do here!</p>
         
      </div>
     </>
