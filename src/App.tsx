0a2f8629e47c8517e2b5f780ae23896eb6720f42
femnixx <femnitality@gmail.com>
2025-09-22 18:50:58 +0700

    bg works

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import background from "./assets/background.jpg"
 
 function App() {
-  
-
   return (
     <>
-       
+      <div className='w-full h-screen bg-cover bg-center' style={{backgroundImage: `url(${background})`}}>
+        <div className='flex items-center justify-center h-full'>
+          <div className='flex bg-red-600'>
+            <p>hi there</p>
+          </div>
+        </div>
+      </div> 
     </>
   )
 }
