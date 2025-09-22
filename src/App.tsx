49acd09d2fdbac5d371295646ab8c1f4fe329841
femnixx <femnitality@gmail.com>
2025-09-22 18:36:18 +0700

    Added background

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,7 @@ function App() {
 
   return (
     <>
-        <h1>Happy Anniversary</h1>
-      <div>
-        <p>I LOVE</p>
-        <h1>YOU</h1>
-      </div>
-      
-      <div>
-        <h1>FIRST MEET</h1>
-        <p>From first meeting because I didn't bring my almameter to the time we met in the open house, my eyes have been opened to how beautiful and kind you are. From just as friends to someone special, I've always loved you, and nothing will ever change that.</p>
-      </div>
+       
     </>
   )
 }
