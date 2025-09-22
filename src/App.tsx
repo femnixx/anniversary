0ed788f8c2b1f51d32f0a12bc9012e8c23176b54
femnixx <femnitality@gmail.com>
2025-09-22 19:16:34 +0700

    idk what else to add but this for now works

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,12 @@ function App() {
      <div className='relative w-full h-screen bg-blue-300'>
         <p className=''>Happy <br /> Anniversary</p>
         <p>Here's to the first year of our relationship</p>
+        {/* decoration like clouds */}
+        <div></div>
         {/* pictures */}
+        <div></div>
+        {/* cinnamoroll pop up */}
+        <p>Thank you for being with me</p>
      </div>
     </>
   )
