644b12c52a11ce8ce3be44c564947e3dd82a5974
femnixx <femnitality@gmail.com>
2025-09-23 14:25:40 +0700

    bluestripesbackground

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import './App.css';
+import cinnamorollking from "./assets/kingcinnamoroll.gif";
 
 gsap.registerPlugin(useGSAP);
 
 function App() {
   return (
     <>
-    {/* splashscreen */}
-    <div className="w-screen h-screen bg-[url('./assets/cinnamy.jpg')] bg-cover bg-center">
-    </div>
-    {/* content */}
-    <div className="w-screen h-screen bg-[url('./assets/bluebackground.jpg')] bg-cover bg-center">
-      <div className='w-full h-full flex flex-col justify-center items-center'>
-          <p></p>
+      {/* splashscreen */}
+      <div className="w-screen h-screen bg-[url('./assets/cinnamy.jpg')] bg-cover bg-center">
+        <div className="w-full h-full flex justify-center items-center">
+          <div className="bg-white rounded-lg shadow-lg">
+            {/* container for video */}
+            <div>
+              <img src={cinnamorollking} alt="king cinnamoroll" className="w-20"/>              
+            </div>
+            <p></p>
+          </div>
+        </div>
       </div>
-    </div>
 
+      {/* content */}
+      <div className="w-screen h-screen bg-[url('./assets/bluebackground.jpg')] bg-cover bg-center">
+        <div className="w-full h-full flex justify-center items-center">
+          <p>Second section</p>
+        </div>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
