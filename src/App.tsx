a6f321d68da12631faa7d20c8e8d2c9694e2b678
femnixx <femnitality@gmail.com>
2025-09-23 14:42:15 +0700

    making welcome card

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,19 @@ function App() {
       {/* splashscreen */}
       <div className="w-screen h-screen bg-[url('./assets/bluerealbackground.jpg')] bg-cover bg-center">
         <div className="w-full h-full flex justify-center items-center">
-          <div className="bg-white rounded-lg shadow-lg">
+          <div className="bg-white rounded-lg shadow-lg flex p-0">
             {/* container for video */}
-            <div>
+            <div className="">
               <img src={cinnamorollking} alt="king cinnamoroll" className="w-20"/>              
             </div>
-            <p></p>
+            {/* content */}
+            <div className="bg-blue-300/50 w-0.5 m-2"></div>
+            <p className="p-2">Welcome</p>
           </div>
         </div>
       </div>
 
-      {/* content */}
+      {/* slide 2 */}
       <div className="w-screen h-screen bg-[url('./assets/bluebackground.jpg')] bg-cover bg-center">
         <div className="w-full h-full flex justify-center items-center">
           <p>Second section</p>
