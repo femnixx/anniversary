6bf58bdbf5d62cfac9011029ac72566fcbd19216
femnixx <femnitality@gmail.com>
2025-09-29 21:53:12 +0700

    Made folder page

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,9 @@ function App() {
             <div className="w-full h-full flex flex-col items-center justify-center">
               <img src={text} alt="" className="max-w-full max-h-full object-contain"/>
             </div>
-              <div className="flex flex-1 items-center justify-center"><img src={cinnamorollpeeking} alt="" className=""/></div>
+              <div className="w-full min-h-max">
+                <div className="flex flex-1 justify-center"><img src={cinnamorollpeeking} alt="" className=""/></div>
+              </div>
       </div>
     </>
   );
