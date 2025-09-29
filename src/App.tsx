cf7fce90883f7d1587fa9ccb76ef0de69184ffa1
femnixx <femnitality@gmail.com>
2025-09-29 21:41:27 +0700

    made cinnamoroll image

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { gsap } from "gsap";
 import './App.css';
 import envelope from "./assets/trans-evelope.png"
 import text from "./assets/text.png"
-
+import cinnamorollpeeking from "./assets/cinnamorollpeeking.png" 
 function App() {
   
   return (
     <>
       {/* splashscreen */}
-      <div className="w-screen h-screen bg-[url('./assets/bluerealbackground.jpg')] bg-cover bg-center p-5">
-        <div className="w-full flex flex-col ">
+      <div className="w-screen h-screen bg-[url('./assets/bluerealbackground.jpg')] bg-cover bg-center p-5 flex flex-col">
           <div className="bg-white/95 rounded-2xl shadow-2xl p-3">
-              <div className="flex justify-center"><img src={envelope} alt="baby blue envelope" className="text-center"/></div>
+              <div className="flex justify-center">
+                <img src={envelope} alt="baby blue envelope"/>
+                </div>
               <div className="-mt-5">
                 <p className="text-center text-blue-400 font-bold">you've got mail ! </p>
                 <div className="flex gap-x-5 w-full justify-center mt-3">
@@ -21,15 +22,14 @@ function App() {
                 </div>
               </div>
           </div>
-          <div>
-            <div className="">
-              <img src={text} alt="" />
+            <div className="w-full h-full flex flex-col items-center justify-center">
+              <img src={text} alt="" className="max-w-full max-h-full object-contain"/>
+              <img src={cinnamorollpeeking} alt="" className="flex flex-1"/>
             </div>
-          </div>
-        </div>
       </div>
     </>
   );
 }
 
 export default App;
+
