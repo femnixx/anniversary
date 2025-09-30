e5df9eb834976928e6101662860bb5f6bd6ec12f
femnixx <femnitality@gmail.com>
2025-09-30 13:12:27 +0700

    Added the structure of the message page

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -4,8 +4,8 @@ const Message = () => {
   return (
     <div className="w-screen h-screen bg-[url('../assets/bluebackground2.jpg')]" style={{ backgroundImage: `url(${bluerealbackground})`, backgroundPosition:"center", backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
         {/* main screen */}
-        <div className="w-full h-full flex items-center justify-center">
-          <div className="bg-white flex justify-center items-center flex-col">
+        <div className="w-full h-full flex items-center justify-center p-3">
+          <div className="bg-white flex justify-center items-center flex-col p-3 gap-y-5">
             <div>
               <p>To My Amore~</p>
             </div>
@@ -13,8 +13,8 @@ const Message = () => {
               <p>Hapy Anniversary!!</p>
             </div>
             {/* main content */}
-            <div>
-
+            <div className="text-center">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ipsam doloribus voluptate unde quaerat aperiam, reprehenderit impedit obcaecati incidunt culpa possimus sapiente quo fugit alias suscipit quisquam quos libero deleniti.</p>
             </div>
             <button>Continue</button>
           </div>
