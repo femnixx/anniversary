41b7e6574fa1539c9900094e3a25906763941331
femnixx <femnitality@gmail.com>
2025-09-30 13:09:29 +0700

    Made the sketch of the page

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -5,23 +5,17 @@ const Message = () => {
     <div className="w-screen h-screen bg-[url('../assets/bluebackground2.jpg')]" style={{ backgroundImage: `url(${bluerealbackground})`, backgroundPosition:"center", backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
         {/* main screen */}
         <div className="w-full h-full flex items-center justify-center">
-          <div className="bg-white border-1 flex justify-center items-center flex-col">
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
-            <p> hi there</p>
+          <div className="bg-white flex justify-center items-center flex-col">
+            <div>
+              <p>To My Amore~</p>
+            </div>
+            <div>
+              <p>Hapy Anniversary!!</p>
+            </div>
+            {/* main content */}
+            <div>
 
+            </div>
           </div>
         </div>
     </div>
