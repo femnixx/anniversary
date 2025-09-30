536e223ab5e191916d17412563d9b2d0057ac33e
femnixx <femnitality@gmail.com>
2025-09-30 09:58:27 +0700

    Added container element to contain the text message

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -1,9 +1,12 @@
-import bluebackground from "../assets/bluebackground.jpg"
+import bluestripesbackground from "../assets/bluestripesbackground.jpg"
 
 const Message = () => {
   return (
-    <div className="w-screen h-screen bg-[url('../assets/bluebackground2.jpg')]" style={{ backgroundImage: `url(${bluebackground})`}}>
-        
+    <div className="w-screen h-screen bg-[url('../assets/bluebackground2.jpg')]" style={{ backgroundImage: `url(${bluestripesbackground})`}}>
+        {/* main screen */}
+        <div>
+
+        </div>
     </div>
   )
 }
