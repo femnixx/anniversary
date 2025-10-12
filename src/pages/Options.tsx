fb74a928590ee6b3df0bfc64cd4a9a2ae7d9bcf1
femnixx <femnitality@gmail.com>
2025-10-12 11:02:12 +0700

    Added an options title assets for the options page

--- src/pages/Options.tsx
+++ src/pages/Options.tsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import bluerealbackground from "../assets/bluerealbackground.jpg"
+import date from "../assets/date.png"
+import moments from "../assets/moments.png"
+import together from "../assets/together.png"
+import compliments from "../assets/compliments.png"
+import title from "../assets/optionstitle.png"
 
 const Options = () => {
   return (
-    <div className='w-screen h-screen' style={{ backgroundImage: `URL(${bluerealbackground})`}}>
-        
+    <div className='w-screen min-h-screen' style={{ backgroundImage: `URL(${bluerealbackground})`}}>
         <div className='flex flex-col justify-center items-center'>
-            <h1 className='mt-[10%]'>Fun Things To Do!</h1>
-            <div className='mt-10 grid grid-cols-1 gap-y-10'>
-                <div className='bg-white/40'>
-
+            <div className='mt-5 p-5'>
+              <img src={title} alt="" />
+            </div>
+            <div className='mt-5 grid grid-cols-1 gap-y-10 m-10'>
+                <div className='border-5 pb-5 border-white bg-white/40 flex flex-col justify-center items-center text-center rounded-xl'>
+                    <div className='flex flex-col justify-center items-center'><img src={date} alt=""  className='w-50'/></div>
+                    <p className='font-semibold'>plan our next date</p>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className='border-5 pb-5 border-white bg-white/40 flex flex-col justify-center items-center text-center rounded-xl'>
+                    <div className='flex flex-col justify-center items-center'><img src={moments} alt=""  className='w-50'/></div>
+                    <p className='font-semibold'>our favorite moments</p>
+                </div>
+                <div className='bg-white/40 border-5 pb-5 border-white flex flex-col justify-center items-center text-center rounded-xl'>
+                    <div className='flex flex-col justify-center items-center'><img src={together} alt=""  className='w-50'/></div>
+                    <p className='font-semibold'>journey of togetherness</p>
+                </div>
+                <div className='bg-white/40 border-5 pb-5 border-white flex flex-col justify-center items-center text-center rounded-xl'>
+                    <div className='flex flex-col justify-center items-center'><img src={compliments} alt=""  className='w-50'/></div>
+                    <p className='font-semibold'>compliments generator</p>
+                </div>
             </div>    
         </div>
     </div>
