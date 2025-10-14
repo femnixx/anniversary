5cddaab244fd158f9488f25b51867b9d48c0ffc4
femnixx <femnitality@gmail.com>
2025-10-14 10:13:49 +0700

    Made the background for the plans page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Landing from './pages/Landing';
-import Message from './pages/Message';
-import Options from './pages/Options';
 import Compliments from './pages/Compliments';
 import Journey from './pages/Journey';
+import Landing from './pages/Landing';
+import Message from './pages/Message';
 import Moments from './pages/Moments';
+import Options from './pages/Options';
 import Plan from './pages/Plan';
 
 
@@ -21,8 +21,8 @@ function App() {
         <Route path='/options' element={<Options />}></Route>
         <Route path='/compliments' element={<Compliments />}/>
         <Route path='/journey' element={<Journey />}/>
-        <Route path='moments' element={<Moments />}/>
-        <Route path='/Plan' element={<Plan />}/>
+        <Route path='/moments' element={<Moments />}/>
+        <Route path='/plan' element={<Plan />}/>
       </Routes>
       </BrowserRouter>
     </>
--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import bluerealbackground from "../assets/bluerealbackground.jpg"
+
 
 const Plan = () => {
   return (
-    <div>Plan</div>
+    <div className="w-screen h-screen">
+      <div className="w-full h-full" style={{backgroundImage: `url(${bluerealbackground})`}}>
+        <div className="w-full h-auto pt-10">
+          <p className="font-bold text-white text-2xl italic text-center">plan our next date!</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
