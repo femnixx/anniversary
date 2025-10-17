3c9e254f89088b5e7a374dcf7a3e1f1e99d508f0
femnixx <femnitality@gmail.com>
2025-10-17 19:52:35 +0700

    Added the pages and some other packages

--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,14 +1,12 @@
 import bluerealbackground from "../assets/bluerealbackground.jpg";
 
 const Plan = () => {
-  const []
   
   return (
     <div className="w-screen h-screen">
       <div className="w-full h-full" style={{backgroundImage: `url(${bluerealbackground})`}}>
         <div className="w-full h-auto pt-10">
           <p className="font-bold text-white text-2xl italic text-center">plan our next date!</p>
-         
 
         </div>
       </div>
