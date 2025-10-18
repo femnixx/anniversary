65c99b22782d607cf56123989626039027190bdf
femnixx <femnitality@gmail.com>
2025-10-19 02:27:29 +0700

    Added mui lab and components

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,20 @@ import Journey from './pages/Journey';
 import Compliments from './pages/Compliments';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <BrowserRouter>
-          <Route path='/' element={<Landing />}></Route>
-          <Route path='/message' element={<Message />}></Route>
-          <Route path='/plan' element={<Plan />}></Route>
-          <Route path='/options' element={<Options />}></Route>
-          <Route path='/moments' element={<Moments />}></Route>
-          <Route path='/journey' element={<Journey />}></Route>
-          <Route path='/compliments' element={<Compliments />}></Route>
+          <Routes>
+            <Route path='/' element={<Landing />}></Route>
+            <Route path='/message' element={<Message />}></Route>
+            <Route path='/plan' element={<Plan />}></Route>
+            <Route path='/options' element={<Options />}></Route>
+            <Route path='/moments' element={<Moments />}></Route>
+            <Route path='/journey' element={<Journey />}></Route>
+            <Route path='/compliments' element={<Compliments />}></Route>
+          </Routes>
         </BrowserRouter>
       </div>
     </>
--- src/pages/Journey.tsx
+++ src/pages/Journey.tsx
@@ -3,14 +3,47 @@ import gsap from 'gsap'
 import { useEffect, useRef } from "react";
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import bluerealbackground from "../assets/bluerealbackground.jpg"
-
-gsap.registerPlugin(ScrollTrigger);
+import Timeline from '@mui/lab/Timeline'
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 const Journey = () => {
   return (
     <div className='w-screen h-screen'>
       <div className='w-full h-full' style={{backgroundImage: `url(${bluerealbackground})`}}>
-        <p>hi</p>
+        <p>scroll down or something</p>
+        <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>Repeat</TimelineContent>
+      </TimelineItem>
+    </Timeline>
       </div>
     </div>
   )
