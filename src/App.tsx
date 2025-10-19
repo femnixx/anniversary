8d20143afbeabc93867ea549390f0826fac2fa2e
femnixx <femnitality@gmail.com>
2025-10-19 15:54:58 +0700

    ALmsot done with moments page

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,28 @@ import Moments from './pages/Moments';
 import Message from './pages/Message';
 import Journey from './pages/Journey';
 import Compliments from './pages/Compliments';
-import { PrimeReactProvider } from "@primereact/core";
-
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+        
 
 function App() {
 
   return (
     <>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Landing />}></Route>
-            <Route path='/message' element={<Message />}></Route>
-            <Route path='/plan' element={<Plan />}></Route>
-            <Route path='/options' element={<Options />}></Route>
-            <Route path='/moments' element={<Moments />}></Route>
-            <Route path='/journey' element={<Journey />}></Route>
-            <Route path='/compliments' element={<Compliments />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <PrimeReactProvider>
+        <div>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Landing />}></Route>
+              <Route path='/message' element={<Message />}></Route>
+              <Route path='/plan' element={<Plan />}></Route>
+              <Route path='/options' element={<Options />}></Route>
+              <Route path='/moments' element={<Moments />}></Route>
+              <Route path='/journey' element={<Journey />}></Route>
+              <Route path='/compliments' element={<Compliments />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </PrimeReactProvider>
     </>
   )
 }
--- src/pages/Moments.tsx
+++ src/pages/Moments.tsx
@@ -1,9 +1,17 @@
 import bluerealbackground from "../assets/bluerealbackground.jpg"
 import pomsit from "../assets/pomsit.png"
-import { usetState } from "react";
+import { FileUpload, type FileUploadSelectEvent } from "primereact/fileupload"
+import { Divider } from 'primereact/divider';
+import { useState } from "react";
+        
+const onUpload = () => {
+  const [files, setFiles] = useState<File[]>([]);
 
-function onUpload() {
-
+  const onSelect = (e: FileUploadSelectEvent) => {
+    const selectedfiles = Array.from(e.files as File[]);
+    setFiles(selectedfiles);
+    
+  }
 }
 
 const Moments = () => {
@@ -14,11 +22,14 @@ const Moments = () => {
       <div className='w-full h-full' style={{backgroundImage: `url(${bluerealbackground})`}}>
         <div className="w-full h-auto pt-20 text-center flex flex-col justify-center items-center">
           <div className="w-full h-auto">
-            <div className="bg-white p-3 m-3 rounded-xl">
+            <div className="bg-white p-3 m-3 rounded-xl flex flex-col gap-y-10">
               <p className="text-center font-semibold text-xl italic pt-5">pick your favorite moment <br /> of us together!</p>
               <div className="w-full justify-center flex flex-1">
-                <img src={pomsit} alt="" className="w-50"/>
+                <img src={pomsit} alt="" className="w-50 -my-15"/>
               </div>
+              <div className="w-full outline-blue-400 outline-dashed outline-2">
+              </div>
+              <FileUpload mode="basic"></FileUpload>
             </div>
           </div>
         </div>
